68a1684ee47c64d51448fa10ec3d8c6b68c2666d
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-09-01 11:44:02 +0530

    Implementing exceptions

---
+++ CustomException/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CustomException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student newStudent = null;
+
+            try
+            {
+                newStudent = new Student();
+                newStudent.StudentName = "Vamshi11";
+
+                ValidateStudent(newStudent);
+            }
+            catch (InvalidStudentNameException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ValidateStudent(Student std)
+        {
+            Regex regex = new Regex("^[a-zA-Z]+$");
+
+            if (!regex.IsMatch(std.StudentName))
+                throw new InvalidStudentNameException(std.StudentName);
+            else
+                Console.WriteLine(std.StudentName);
+        }
+    }
+}
---
+++ CustomException/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomException
+{
+    internal class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    [Serializable]
+    class InvalidStudentNameException : Exception
+    {
+        public InvalidStudentNameException() { }
+
+        public InvalidStudentNameException(string name)
+            : base(String.Format("Invalid Student Name: {0}", name))
+        {
+
+        }
+    }
+}
+
